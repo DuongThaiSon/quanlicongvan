88e826347af27129fbb3bfe1a37c6a6858975515
DuongThaiSon <sontd@leotive.com>
2019-10-21 19:01:19 +0700

    first push

---
+++ public/pmhdv/js/script.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+    $("#tra-loi1").click(function () {
+        $("#rep1").toggle();
+    });
+    $("#tra-loi2").click(function () {
+        $("#rep2").toggle();
+    });
+
+    $(".continue-js").click(function () {
+        $("#create-cv").addClass("d-none");
+        $(".form-themcv").addClass("d-block");
+    });
+
+    $(".back-js").click(function () {
+        $(".form-themcv").removeClass("d-block");
+        $("#create-cv").removeClass("d-none");
+    });
+
+    $('input[name="username"],input[name="pass"]').focus(function () {
+        $(this).prev().animate({
+            'opacity': '1'
+        }, 200);
+    });
+    $('input[name="username"],input[name="pass"]').blur(function () {
+        $(this).prev().animate({
+            'opacity': '.5'
+        }, 200);
+    });
+    $('input[name="username"],input[name="pass"]').keyup(function () {
+        if (!$(this).val() == '') {
+            $(this).next().animate({
+                'opacity': '1',
+                'right': '30'
+            }, 200);
+        } else {
+            $(this).next().animate({
+                'opacity': '0',
+                'right': '20'
+            }, 200);
+        }
+    });
+
+    $('.login-signup a').click(function () {
+        $('form').animate({
+            height: "toggle",
+            opacity: "toggle"
+        }, "slow")
+    });
+
+    $('.btn-toggle').click(function () {
+        $('.master').toggleClass('is-collapsed');
+    });
+
+
+    ClassicEditor
+        .create(document.querySelector('#editor'))
+        .catch(error => {
+            console.error(error);
+        });
+});
---
+++ routes/web.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('index', function () {
+//     return view('viewer.page.trangchu');
+// });
+
+Route::get('login',function(){
+	return view('login');
+});	
+Route::post('login','UserController@postDangNhap')->name('post-login');
+
+Route::group(['prefix'=>'viewer'],function(){
+	Route::group(['prefix'=>'congvandi'],function(){
+		// Route::get('danhsach','CongVanDiController@getDSCVDi');
+
+		Route::get('themmoi','CongVanDiController@getThemCongVan');
+
+		Route::post('themmoi','CongVanDiController@postThemCongVan')->name('them-cv');
+
+	});
+	Route::group(['prefix'=>'congvanden'],function(){
+		Route::get('danhsach','CongVanDenController@getDSCVDen');
+
+		Route::get('chitiet/{id}','CongVanDenController@getChiTiet');
+
+		Route::get('timcongvanden','CongVanDenController@getTimCongVanDen')->name('get-timcvden');
+		// Route::get('them','TheLoaiController@getThem');
+
+		// Route::post('them','TheLoaiController@postThem');
+
+		// Route::get('sua/{id}','TheLoaiController@getSua');
+
+		// Route::post('sua/{id}','TheLoaiController@postSua');
+
+		// Route::get('xoa/{id}','TheLoaiController@getXoa');
+	
+	});
+
+	Route::group(['prefix'=>'congvan'],function(){
+
+		Route::get('danhsach','CongVanController@getDanhSach');
+
+		Route::get('taomoi','CongVanController@getTaoMoi');
+
+		Route::post('taomoi','CongVanController@postTaoMoi')->name('post-taocv');
+
+		Route::get('timcongvan','CongVanController@getTimCongVan')->name('get-timcv');
+	});
+
+	Route::group(['prefix'=>'ajax'],function(){
+		Route::get('user/{idbophannhan}','AjaxController@getUser');
+	});
+});
+
+

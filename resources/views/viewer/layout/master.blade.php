de5b8ec71c4833dba576ebedcabb823151b3b0c2
dinhtuan5160 <55198575+dinhtuan5160@users.noreply.github.com>
2019-11-22 20:18:21 +0700

    lag

--- resources/views/viewer/layout/master.blade.php
+++ resources/views/viewer/layout/master.blade.php
@@ -14,15 +14,18 @@
         integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <link rel="stylesheet" href="pmhdv/css/themify-icons.css">
     <link rel="stylesheet" href="pmhdv/css/style.css">
+    <script src="pmhdv/js/jquery.min.js"></script>
+    <script src="pmhdv/js/popper.min.js"></script>
+    <script src="pmhdv/js/bootstrap.min.js"></script>
+    <script src="pmhdv/js/ckeditor.js"></script>
+    <script src="pmhdv/js/owl.carousel.js"></script>
+    <script src="pmhdv/js/Chart.bundle.min.js"></script>
+    <script src="pmhdv/js/script.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.10/css/select2.min.css" rel="stylesheet">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.10/js/select2.min.js"></script>
-    <script src="pmhdv/js/ckeditor.js"></script>
-    <script src="pmhdv/js/owl.carousel.js"></script>
-    <script src="pmhdv/js/Chart.bundle.min.js"></script>
-    <script src="pmhdv/js/script.js"></script>
     @yield('title')
     
     
@@ -34,14 +37,8 @@
 
     
     <section class="master">
-        <div class="row">
-            <div class="col-lg-2 fix-col text-left">
-                @include('viewer.layout.menu')
-            </div>
-            <div class="col-lg-10">
-                @yield('content')
-            </div>
-        </div>
+        @include('viewer.layout.menu')
+        @yield('content')
         
     </section>
     @yield('script')
